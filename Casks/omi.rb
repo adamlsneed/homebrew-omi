001eb4dc712c262efc78e0a792683857c4974cce
Adam Sneed <adam@macnano.localdomain>
2026-06-15 19:11:57 -0500

    omi 0.1.5

--- Casks/omi.rb
+++ Casks/omi.rb
@@ -1,8 +1,8 @@
 cask "omi" do
-  version "0.1.4"
-  sha256 "c956bc3eec62150c241c7d88a8d0b7cc81343324faeb2d0e097814e757567b26"
+  version "0.1.5"
+  sha256 "380b68ec8dc1472df50e94cc82195898d9e6dc924bd4ca3a1418b5a09b840b8a"
 
-  url "https://github.com/adamlsneed/omi/releases/download/desktop-fork-v0.1.4/omi-desktop-0.1.4.zip"
+  url "https://github.com/adamlsneed/omi/releases/download/desktop-fork-v0.1.5/omi-desktop-0.1.5.zip"
   name "Omi Dev"
   desc "Adam's Omi desktop fork (notarized)"
   homepage "https://github.com/adamlsneed/omi"
